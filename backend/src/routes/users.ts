78aa90c690216667545abad0e18882413398f5ff
hwrdyen <hwrdpvt@gmail.com>
2024-01-02 21:21:33 -0500

    Completed first draft of registration form

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -46,7 +46,7 @@ router.post(
         maxAge: 86400000, // in ms
       });
 
-      return res.sendStatus(200);
+      return res.status(200).send({ message: "User registered successfully!" });
     } catch (error) {
       console.log(error);
       res.status(500).send({ message: "Something went wrong" });
