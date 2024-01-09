d0dc810f233100aa9f63bd7d95345c07d24ac9c5
hwrdyen <hwrdpvt@gmail.com>
2024-01-09 00:04:29 -0500

    Added max-price filter functionality

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -7,6 +7,7 @@ import Pagination from "../components/Pagination";
 import StarRatingFilter from "../components/StarRatingFilter";
 import HotelTypesFilter from "../components/HotelTypesFilter";
 import FacilitiesFilter from "../components/FacilitiesFilter";
+import PriceFilter from "../components/PriceFilter";
 
 const Search = () => {
   const search = useSearchContext();
@@ -16,6 +17,7 @@ const Search = () => {
   const [selectedStars, setSelectedStars] = useState<string[]>([]);
   const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
   const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
+  const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
 
   const searchParams = {
     destination: search.destination,
@@ -27,6 +29,7 @@ const Search = () => {
     stars: selectedStars,
     types: selectedHotelTypes,
     facilities: selectedFacilities,
+    maxPrice: selectedPrice?.toString(),
   };
 
   // anytime we change our searchParams,
@@ -95,6 +98,11 @@ const Search = () => {
           <FacilitiesFilter
             selectedFacilities={selectedFacilities}
             onChange={handleFacilityChange}
+          />
+
+          <PriceFilter
+            selectedPrice={selectedPrice}
+            onChange={(value?: number) => setSelectedPrice(value)}
           />
         </div>
       </div>
