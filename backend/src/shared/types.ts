597d68625828325f8c69b88c3901c61f1bec6f7f
hwrdyen <hwrdpvt@gmail.com>
2024-01-16 21:03:47 -0500

    Created stripe payment intent API endpoint

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -32,3 +32,9 @@ export type UserType = {
   firstName: string;
   lastName: string;
 };
+
+export type PaymentIntentResponse = {
+  paymentIntentId: string;
+  clientSecret: string;
+  totalCost: number;
+};
